fae3c89cd85ea4a514e1bdfa50976668ead46591
LittleBear-6w6 <57203646+LittleBear-6w6@users.noreply.github.com>
2021-11-21 12:03:11 +0900

    Initial commit

---
+++ app/components/TheHeader.vue
@@ -0,0 +1,25 @@
+<template>
+    <el-menu mode="horizontal" :router="true">
+        <el-menu-item index="1" style="pointer-events:none;">
+            Easy Web App Demo
+        </el-menu-item>
+
+        <no-ssr>
+            <el-menu-item index="2" style="float: right;pointer-events:none;" v-if="user">
+                <span>{{user.id}}</span>
+            </el-menu-item>
+            <el-menu-item index="2" style="float: right;" :route="{ path: '/' }" v-else>
+                <span>ログイン</span>
+            </el-menu-item>
+        </no-ssr>
+    </el-menu>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+    computed:{
+        ...mapGetters(['user'])
+    }
+}
+</script>
